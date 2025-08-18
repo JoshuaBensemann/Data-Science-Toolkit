835e65b6f56e0e0b457b7d36c96664bff3954a2a
Joshua Bensemann <joshua.bensemann@gyldagency.com>
2025-08-19 10:03:04 +1200

    basic loaded started

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Install command for development
+
 pip install -e .  
 
 # Commands for pre-commit
-pip install pre-commit    
-pre-commit install     
+
+pip install pre-commit
+pre-commit install
--- ds_toolkit/tools/loaders.py
+++ ds_toolkit/tools/loaders.py
@@ -1,1 +1,15 @@
+import pandas as pd
 
+
+def load_dataframe(file_path: str, **kwargs) -> pd.DataFrame:
+    df: pd.DataFrame | None = None
+
+    if file_path.endswith(".csv"):
+        df = pd.read_csv(file_path, **kwargs)
+
+    if df is not None:
+        print(f"Loaded dataframe has {df.shape[0]} rows and {df.shape[1]} features.")
+    else:
+        print("File at {file_path} could not be loaded as a dataframe!")
+
+    return df
